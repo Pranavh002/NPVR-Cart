5c6a11e81011f7fad7572deb5985bb3cd1514016
pranavh002 <pranavh002@gmail.com>
2024-10-18 11:31:29 +0530

    Get Single Product API

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,5 +1,8 @@
 const ProductModel=require('../models/productModel')
 
+
+//Get Single Product API- '/api/v1/product'
+
 exports.getProducts=async (req, res, next)=>{
 
     const products= await ProductModel.find({})       //takes the entire model if given empty{} inside find method
@@ -10,9 +13,25 @@ exports.getProducts=async (req, res, next)=>{
 
 }
 
-exports.getSingleProduct=(req, res, next)=>{
-    res.json({
-        sucess:true,
-        message:'Get Single product working!'
-    })
+//Get Single Product API- '/api/v1/product/:id'
+
+exports.getSingleProduct=async(req, res, next)=>{
+   
+    try{
+        const product = await ProductModel.findById(req.params.id);
+        res.json({
+            sucess:true,
+            product
+        })
+
+    }
+    catch(error){
+        res.status(404).json({
+            sucess:false,
+            message: "Product Not found"
+        })
+
+
+    }
+
 }
