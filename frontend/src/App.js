f76aad7e888e5daa55ea572684b494784238eeb0
pranavh002 <pranavh002@gmail.com>
2024-10-18 19:38:22 +0530

    Creating Header, Footer components and Breaking Page to Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import Header from './components/Header';
 import Home from './pages/Home'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Home/>
+      <Footer/>
     </div>
   );
 }
