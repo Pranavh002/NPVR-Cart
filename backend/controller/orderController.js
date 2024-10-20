6cccca37c2119129a1cdf09cf6eec1b9544aaba8
pranavh002 <pranavh002@gmail.com>
2024-10-20 21:55:05 +0530

    Updating Product Stock

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -1,20 +1,28 @@
-const orderModel=require('../models/orderModel')
+const orderModel = require('../models/orderModel')
+const productModel = require('../models/productModel')
 
 
 //Create Order - '/api/v1/order'
-exports.createOrder=async(req, res, next)=>{
+exports.createOrder = async (req, res, next) => {
     // console.log(req.body, 'DATA');
 
-    const cartItems=req.body;
-    const amount=Number(cartItems.reduce((acc, item)=>(acc+ item.product.price * item.qty), 0)).toFixed(2);
-    const status="pending";
+    const cartItems = req.body;
+    const amount = Number(cartItems.reduce((acc, item) => (acc + item.product.price * item.qty), 0)).toFixed(2);
+    const status = "pending";
 
     // console.log("amount",amount)
 
-    const order=await orderModel.create({cartItems, amount, status})
+    const order = await orderModel.create({ cartItems, amount, status });
+
+    //updating product stock
+    cartItems.forEach(async (item) => {
+        const product = await productModel.findById(item.product._id);
+        product.stock = product.stock - item.qty;
+        await product.save();
+    })
 
     res.json({
-        sucess:true,
+        sucess: true,
         order
     })
 
